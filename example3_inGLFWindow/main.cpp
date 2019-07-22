fe226e2fb9d34b3334bbc37a94ba68a489cfe031
Ingo Wald <iwald@nvidia.com>
2019-07-22 13:55:39 -0600

    first example that builds a BLAS over triangles

--- example3_inGLFWindow/main.cpp
+++ example3_inGLFWindow/main.cpp
@@ -62,8 +62,6 @@ namespace osc {
       glBindTexture(GL_TEXTURE_2D, fbTexture);
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-      //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-      //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
       
       glDisable(GL_DEPTH_TEST);
 
