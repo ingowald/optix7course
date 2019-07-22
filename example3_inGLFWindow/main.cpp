9d374c8960d78708765231e035a78e49270adb34
Ingo Wald <ingowald@gmail.com>
2019-07-21 22:12:15 -0600

    example 3 displays the test frame

--- example3_inGLFWindow/main.cpp
+++ example3_inGLFWindow/main.cpp
@@ -22,6 +22,7 @@
 
 // our helper library for window handling
 #include "glfWindow/GLFWindow.h"
+#include <GL/gl.h>
 
 /*! \namespace osc - Optix Siggraph Course */
 namespace osc {
@@ -37,10 +38,56 @@ namespace osc {
       sample.render();
     }
     
+    GLuint fbTexture {0};
+    
     virtual void draw() override
     {
       sample.downloadPixels(pixels.data());
+      if (fbTexture == 0)
+        glGenTextures(1, &fbTexture);
       
+      glBindTexture(GL_TEXTURE_2D, fbTexture);
+      GLenum texFormat = GL_RGBA;
+      GLenum texelType = GL_UNSIGNED_BYTE;
+      glTexImage2D(GL_TEXTURE_2D, 0, texFormat, fbSize.x, fbSize.y, 0, GL_RGBA,
+                   texelType, pixels.data());
+
+      glDisable(GL_LIGHTING);
+      glColor3f(1, 1, 1);
+
+      glMatrixMode(GL_MODELVIEW);
+      glLoadIdentity();
+
+      glEnable(GL_TEXTURE_2D);
+      glBindTexture(GL_TEXTURE_2D, fbTexture);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+      
+      glDisable(GL_DEPTH_TEST);
+
+      glViewport(0, 0, fbSize.x, fbSize.y);
+
+      glMatrixMode(GL_PROJECTION);
+      glLoadIdentity();
+      glOrtho(0.0, fbSize.x, 0.0, fbSize.y, -1.0, 1.0);
+
+      glBegin(GL_QUADS);
+      {
+        glTexCoord2f(0.f, 0.f);
+        glVertex3f(0, 0, 0);
+      
+        glTexCoord2f(0.f, 1.f);
+        glVertex3f(0, fbSize.y, 0);
+      
+        glTexCoord2f(1.f, 1.f);
+        glVertex3f(fbSize.x, fbSize.y, 0);
+      
+        glTexCoord2f(1.f, 0.f);
+        glVertex3f(fbSize.x, 0, 0);
+      }
+      glEnd();
     }
     
     virtual void resize(const vec2i &newSize) 
