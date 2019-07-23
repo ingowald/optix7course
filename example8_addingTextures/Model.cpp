3d134c478873836f5ac7986d880f258f25d83073
Ingo Wald <ingowald@gmail.com>
2019-07-23 13:23:57 -0600

    starting on texture example

---
+++ example8_addingTextures/Model.cpp
@@ -0,0 +1,154 @@
+// ======================================================================================
+//  Copyright (c) 2019 NVIDIA Corporation.  All rights reserved.
+//
+//  NVIDIA Corporation and its licensors retain all intellectual property and proprietary
+//  rights in and to this software, related documentation and any modifications thereto.
+//  Any use, reproduction, disclosure or distribution of this software and related
+//  documentation without an express license agreement from NVIDIA Corporation is strictly
+//  prohibited.
+//
+//  TO THE MAXIMUM EXTENT PERMITTED BY APPLICABLE LAW, THIS SOFTWARE IS PROVIDED *AS IS*
+//  AND NVIDIA AND ITS SUPPLIERS DISCLAIM ALL WARRANTIES, EITHER EXPRESS OR IMPLIED,
+//  INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+//  PARTICULAR PURPOSE.  IN NO EVENT SHALL NVIDIA OR ITS SUPPLIERS BE LIABLE FOR ANY
+//  SPECIAL, INCIDENTAL, INDIRECT, OR CONSEQUENTIAL DAMAGES WHATSOEVER (INCLUDING, WITHOUT
+//  LIMITATION, DAMAGES FOR LOSS OF BUSINESS PROFITS, BUSINESS INTERRUPTION, LOSS OF
+//  BUSINESS INFORMATION, OR ANY OTHER PECUNIARY LOSS) ARISING OUT OF THE USE OF OR
+//  INABILITY TO USE THIS SOFTWARE, EVEN IF NVIDIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+//  SUCH DAMAGES
+// ======================================================================================
+
+#include "Model.h"
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "3rdParty/tiny_obj_loader.h"
+//std
+#include <set>
+
+namespace std {
+  inline bool operator<(const tinyobj::index_t &a,
+                        const tinyobj::index_t &b)
+  {
+    if (a.vertex_index < b.vertex_index) return true;
+    if (a.vertex_index > b.vertex_index) return false;
+    
+    if (a.normal_index < b.normal_index) return true;
+    if (a.normal_index > b.normal_index) return false;
+    
+    if (a.texcoord_index < b.texcoord_index) return true;
+    if (a.texcoord_index > b.texcoord_index) return false;
+    
+    return false;
+  }
+}
+
+/*! \namespace osc - Optix Siggraph Course */
+namespace osc {
+  
+
+
+  /*! find vertex with given position, normal, texcoord, and return
+      its vertex ID, or, if it doesn't exit, add it to the mesh, and
+      its just-created index */
+  int addVertex(TriangleMesh *mesh,
+                tinyobj::attrib_t &attributes,
+                const tinyobj::index_t &idx,
+                std::map<tinyobj::index_t,int> &knownVertices)
+  {
+    if (knownVertices.find(idx) != knownVertices.end())
+      return knownVertices[idx];
+
+    const vec3f *vertex_array   = (const vec3f*)attributes.vertices.data();
+    const vec3f *normal_array   = (const vec3f*)attributes.normals.data();
+    const vec2f *texcoord_array = (const vec2f*)attributes.texcoords.data();
+    
+    int newID = mesh->vertex.size();
+    knownVertices[idx] = newID;
+
+    mesh->vertex.push_back(vertex_array[idx.vertex_index]);
+    if (idx.normal_index >= 0) {
+      while (mesh->normal.size() < mesh->vertex.size())
+        mesh->normal.push_back(normal_array[idx.normal_index]);
+    }
+    if (idx.texcoord_index >= 0) {
+      while (mesh->texcoord.size() < mesh->vertex.size())
+        mesh->texcoord.push_back(texcoord_array[idx.texcoord_index]);
+    }
+
+    // just for sanity's sake:
+    if (mesh->texcoord.size() > 0)
+      mesh->texcoord.resize(mesh->vertex.size());
+    // just for sanity's sake:
+    if (mesh->normal.size() > 0)
+      mesh->normal.resize(mesh->vertex.size());
+    
+    return newID;
+  }
+  
+  Model *loadOBJ(const std::string &objFile)
+  {
+    Model *model = new Model;
+
+    const std::string mtlDir
+      = objFile.substr(0,objFile.rfind('/')+1);
+    PRINT(mtlDir);
+    
+    tinyobj::attrib_t attributes;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+    std::string err = "";
+
+    bool readOK
+      = tinyobj::LoadObj(&attributes,
+                         &shapes,
+                         &materials,
+                         &err,
+                         objFile.c_str(),
+                         mtlDir.c_str(),
+                         /* triangulate */true);
+    if (!readOK) {
+      throw std::runtime_error("Could not read OBJ model from "+objFile+":"+mtlDir+" : "+err);
+    }
+
+    for (auto vtx : attributes.vertices)
+      model->bounds.extend(vtx);
+    
+    if (materials.empty())
+      throw std::runtime_error("could not parse materials ...");
+
+    std::cout << "Done loading obj file - found " << shapes.size() << " shapes with " << materials.size() << " materials" << std::endl;
+    for (int shapeID=0;shapeID<(int)shapes.size();shapeID++) {
+      tinyobj::shape_t &shape = shapes[shapeID];
+
+      std::set<int> materialIDs;
+      for (auto faceMatID : shape.mesh.material_ids)
+        materialIDs.insert(faceMatID);
+      
+      std::map<tinyobj::index_t,int> knownVertices;
+      
+      for (int materialID : materialIDs) {
+        TriangleMesh *mesh = new TriangleMesh;
+        
+        for (int faceID=0;faceID<shape.mesh.material_ids.size();faceID++) {
+          if (shape.mesh.material_ids[faceID] != materialID) continue;
+          tinyobj::index_t idx0 = shape.mesh.indices[3*faceID+0];
+          tinyobj::index_t idx1 = shape.mesh.indices[3*faceID+1];
+          tinyobj::index_t idx2 = shape.mesh.indices[3*faceID+2];
+          
+          vec3i idx(addVertex(mesh, attributes, idx0, knownVertices),
+                    addVertex(mesh, attributes, idx1, knownVertices),
+                    addVertex(mesh, attributes, idx2, knownVertices));
+          mesh->index.push_back(idx);
+          mesh->diffuse = (const vec3f&)materials[materialID].diffuse;
+        }
+
+        if (mesh->vertex.empty())
+          delete mesh;
+        else
+          model->meshes.push_back(mesh);
+      }
+    }
+
+    std::cout << "created a total of " << model->meshes.size() << " meshes" << std::endl;
+    return model;
+  }
+}
---
+++ example8_addingTextures/main.cpp
@@ -0,0 +1,142 @@
+// ======================================================================================
+//  Copyright (c) 2019 NVIDIA Corporation.  All rights reserved.
+//
+//  NVIDIA Corporation and its licensors retain all intellectual property and proprietary
+//  rights in and to this software, related documentation and any modifications thereto.
+//  Any use, reproduction, disclosure or distribution of this software and related
+//  documentation without an express license agreement from NVIDIA Corporation is strictly
+//  prohibited.
+//
+//  TO THE MAXIMUM EXTENT PERMITTED BY APPLICABLE LAW, THIS SOFTWARE IS PROVIDED *AS IS*
+//  AND NVIDIA AND ITS SUPPLIERS DISCLAIM ALL WARRANTIES, EITHER EXPRESS OR IMPLIED,
+//  INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+//  PARTICULAR PURPOSE.  IN NO EVENT SHALL NVIDIA OR ITS SUPPLIERS BE LIABLE FOR ANY
+//  SPECIAL, INCIDENTAL, INDIRECT, OR CONSEQUENTIAL DAMAGES WHATSOEVER (INCLUDING, WITHOUT
+//  LIMITATION, DAMAGES FOR LOSS OF BUSINESS PROFITS, BUSINESS INTERRUPTION, LOSS OF
+//  BUSINESS INFORMATION, OR ANY OTHER PECUNIARY LOSS) ARISING OUT OF THE USE OF OR
+//  INABILITY TO USE THIS SOFTWARE, EVEN IF NVIDIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+//  SUCH DAMAGES
+// ======================================================================================
+
+#include "SampleRenderer.h"
+
+// our helper library for window handling
+#include "glfWindow/GLFWindow.h"
+#include <GL/gl.h>
+
+/*! \namespace osc - Optix Siggraph Course */
+namespace osc {
+
+  struct SampleWindow : public GLFWindow
+  {
+    SampleWindow(const std::string &title,
+                 const Model *model,
+                 const Camera &camera)
+      : GLFWindow(title),
+        sample(model)
+    {
+      sample.setCamera(camera);
+    }
+    
+    virtual void render() override
+    {
+      sample.render();
+    }
+    
+    GLuint fbTexture {0};
+    
+    virtual void draw() override
+    {
+      sample.downloadPixels(pixels.data());
+      if (fbTexture == 0)
+        glGenTextures(1, &fbTexture);
+      
+      glBindTexture(GL_TEXTURE_2D, fbTexture);
+      GLenum texFormat = GL_RGBA;
+      GLenum texelType = GL_UNSIGNED_BYTE;
+      glTexImage2D(GL_TEXTURE_2D, 0, texFormat, fbSize.x, fbSize.y, 0, GL_RGBA,
+                   texelType, pixels.data());
+
+      glDisable(GL_LIGHTING);
+      glColor3f(1, 1, 1);
+
+      glMatrixMode(GL_MODELVIEW);
+      glLoadIdentity();
+
+      glEnable(GL_TEXTURE_2D);
+      glBindTexture(GL_TEXTURE_2D, fbTexture);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+      
+      glDisable(GL_DEPTH_TEST);
+
+      glViewport(0, 0, fbSize.x, fbSize.y);
+
+      glMatrixMode(GL_PROJECTION);
+      glLoadIdentity();
+      glOrtho(0.f, (float)fbSize.x, 0.f, (float)fbSize.y, -1.f, 1.f);
+
+      glBegin(GL_QUADS);
+      {
+        glTexCoord2f(0.f, 0.f);
+        glVertex3f(0.f, 0.f, 0.f);
+      
+        glTexCoord2f(0.f, 1.f);
+        glVertex3f(0.f, (float)fbSize.y, 0.f);
+      
+        glTexCoord2f(1.f, 1.f);
+        glVertex3f((float)fbSize.x, (float)fbSize.y, 0.f);
+      
+        glTexCoord2f(1.f, 0.f);
+        glVertex3f((float)fbSize.x, 0.f, 0.f);
+      }
+      glEnd();
+    }
+    
+    virtual void resize(const vec2i &newSize) 
+    {
+      fbSize = newSize;
+      sample.resize(newSize);
+      pixels.resize(newSize.x*newSize.y);
+    }
+
+    vec2i                 fbSize;
+    SampleRenderer        sample;
+    std::vector<uint32_t> pixels;
+  };
+  
+  
+  /*! main entry point to this example - initialy optix, print hello
+    world, then exit */
+  extern "C" int main(int ac, char **av)
+  {
+    try {
+      Model *model = loadOBJ(
+#ifdef _WIN32
+      // on windows, visual studio creates _two_ levels of build dir
+      // (x86/Release)
+      "../../sponza.obj"
+#else
+      // on linux, common practice is to have ONE level of build dir
+      // (say, <project>/build/)...
+      "../sponza.obj"
+#endif
+                             );
+      
+      Camera camera = { /*from*/vec3f(-1293.07f, 154.681f, -0.7304f),
+                        /* at */vec3f(-907.108f, 205.875f, -0.0267f),
+                        /* up */vec3f(0.f,1.f,0.f) };
+      SampleWindow *window = new SampleWindow("Optix 7 Course Example",
+                                              model,camera);
+      window->run();
+      
+    } catch (std::runtime_error e) {
+      std::cout << GDT_TERMINAL_RED << "FATAL ERROR: " << e.what()
+                << GDT_TERMINAL_DEFAULT << std::endl;
+      std::cout << "Did you forget to copy sponza.obj and sponza.mtl into your project dir?" << std::endl;
+        exit(1);
+    }
+    return 0;
+  }
+  
+} // ::osc
