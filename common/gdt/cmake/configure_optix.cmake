869cb91dd222c717b112ba22a1de298422161bac
Ingo Wald <ingowald@gmail.com>
2019-07-31 11:40:32 -0700

    copying in glfw (rather than submodule)

--- common/gdt/cmake/configure_optix.cmake
+++ common/gdt/cmake/configure_optix.cmake
@@ -21,7 +21,7 @@ set(CMAKE_MODULE_PATH
   )
 
 find_package(CUDA REQUIRED)
-find_package(OptiX REQUIRED VERSION 6.6)
+find_package(OptiX REQUIRED VERSION 7.0)
 
 #include_directories(${CUDA_TOOLKIT_INCLUDE})
 if (CUDA_TOOLKIT_ROOT_DIR)
