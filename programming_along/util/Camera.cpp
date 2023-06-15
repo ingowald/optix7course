d717e3801a8845c6fefd4408e0d36e73d509cd49
Tim Lobner <Tare.noN@web.de>
2023-06-15 11:40:23 +0200

    boiler plate camera code

--- programming_along/util/Camera.cpp
+++ programming_along/util/Camera.cpp
@@ -2,7 +2,58 @@
 
 #include "Camera.h"
 
-Camera::Camera()
+Camera::Camera(const vec3f& eye /* = vec3f(0.f, 0.f, 0.f)*/, 
+	const vec3f& at /* = vec3f(0.f, 0.f, -1.f)*/, 
+	const vec3f& up /* = vec3f(0.f, 1.f, 0.f)*/,
+	const float& fovy /* = 0.66f*/)
+{
+	Eye = eye;
+	At = at;
+	Up = up;
+	Fovy = fovy;
+}
+
+Camera::~Camera()
 {
 
+}
+
+void Camera::SetEye(const vec3f& eye)
+{
+	Eye = eye;
+}
+
+vec3f Camera::GetEye() const
+{
+	return Eye;
+}
+
+void Camera::SetAt(const vec3f& at)
+{
+	At = at;
+}
+
+vec3f Camera::GetAt() const
+{
+	return At;
+}
+
+void Camera::SetUp(const vec3f& up)
+{
+	Up = up;
+}
+
+vec3f Camera::GetUp() const
+{
+	return Up;
+}
+
+void Camera::SetFovy(const float& fovy)
+{
+	Fovy = fovy;
+}
+
+float Camera::GetFovy() const
+{
+	return Fovy;
 }
--- programming_along/util/Camera.h
+++ programming_along/util/Camera.h
@@ -2,10 +2,32 @@
 
 #include "gdt/math/vec.h"
 
-using namespace osc;
+using namespace gdt;
 
 class Camera
 {
-	Camera(const vec3f& eye = vec3f(0.f, 0.f, 0.f), const vec3f& at, const vec3f& up)
+	Camera(const vec3f& eye = vec3f(0.f, 0.f, 0.f), 
+		const vec3f& at = vec3f(0.f, 0.f, -1.f), 
+		const vec3f& up = vec3f(0.f, 1.f, 0.f),
+		const float& fovy = 0.66f);
 	~Camera();
+
+	void SetEye(const vec3f& eye);
+	vec3f GetEye() const;
+
+	void SetAt(const vec3f& at);
+	vec3f GetAt() const;
+
+	void SetUp(const vec3f& up);
+	vec3f GetUp() const;
+
+	void SetFovy(const float& fovy);
+	float GetFovy() const;
+
+private:
+	vec3f Eye;
+	vec3f At;
+	vec3f Up;
+
+	float Fovy;
 };
