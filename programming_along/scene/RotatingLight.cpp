3ac7106d88809f2bfe93133c86d2730ead51c4a6
Tim Lobner <Tare.noN@web.de>
2023-06-21 17:07:48 +0200

    fixed compilation errors

--- programming_along/scene/RotatingLight.cpp
+++ programming_along/scene/RotatingLight.cpp
@@ -1,7 +1,7 @@
 
 #include "RotatingLight.h"
 
-RotatingLight::RotatingLight(const vec3f& location, float rotationSpeed = 1.f, float rotationRadius = 1.f)
+RotatingLight::RotatingLight(const vec3f& location, float rotationSpeed /* = 1.f*/, float rotationRadius /* = 1.f */ )
 {
 	Location = location;
 	RotationSpeed = rotationSpeed;
@@ -10,8 +10,6 @@ RotatingLight::RotatingLight(const vec3f& location, float rotationSpeed = 1.f, f
 
 void RotatingLight::Tick(const float& deltaTime_seconds)
 {
-	TotalTime += deltaTime_seconds;
-
 	vec3f location = Location;
 	location.x += sin(deltaTime_seconds * RotationSpeed) * RotationRadius;
 	location.z += cos(deltaTime_seconds * RotationSpeed) * RotationRadius;
